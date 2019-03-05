95f4a485117a98607a403d29ce4d7896738e3347
Admin <admin@MAC26.local>
2019-03-05 11:49:53 +0700

    Initial Commit

---
+++ mid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mid
+//
+//  Created by Admin on 5/3/2562 BE.
+//  Copyright © 2562 supawitch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
